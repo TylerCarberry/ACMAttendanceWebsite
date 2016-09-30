2421112ba8df9182afce59025bd821f52221e043
tylercarberry <tylercarberry@gmail.com>
2016-09-30 19:04:05 -0400

    Remove unused code

--- public/Scripts/FireBase.js
+++ public/Scripts/FireBase.js
@@ -1,6 +1,3 @@
-// This is my first time ever writing Javascript or HTML from scratch.
-// Sorry if it's hard to read.
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAQCEhRC7wf8iCyEW1puwteuWMP5EP7U14",
@@ -24,8 +21,8 @@ new_members_ref.on('value', function(snapshot) {
     document.getElementById("first_meeting").innerHTML = "Number of new members: " + first_value;
 });
 
-var new_members_ref = firebase.database().ref('poll_url/');
-new_members_ref.on('value', function(snapshot) {
+var poll_url_ref = firebase.database().ref('poll_url/');
+poll_url_ref.on('value', function(snapshot) {
     var poll_url = snapshot.val();
     document.getElementById("poll_url").innerHTML = poll_url;
     $("#poll_qr").attr("src", poll_url+".qr");
@@ -65,10 +62,7 @@ function drawChartPoll() {
             }
         }
 
-        console.log(results_array);
-
         var data = google.visualization.arrayToDataTable(results_array);
-
         var options = {
           title: poll_question
         };
@@ -79,46 +73,6 @@ function drawChartPoll() {
         chart.draw(data, options);
     }
 }
-
-
-function drawChart() {
-    console.log("In draw chart");
-
-    if (vote_web > 0 || vote_android > 0 || vote_game > 0 || vote_cpc > 0 || vote_ai > 0 || vote_acmw > 0) {
-        var results_array = [['What is the best committee?', 'Votes']];
-
-        if(vote_web > 0)
-            results_array.push(['Web Dev', vote_web]);
-        if(vote_android > 0)
-            results_array.push(['Android', vote_android]);
-        if(vote_game > 0)
-            results_array.push(['Animation/Game Design', vote_game]);
-        if(vote_cpc > 0)
-            results_array.push(['CPC', vote_cpc]);
-        if(vote_ai > 0)
-            results_array.push(['AI', vote_ai]);
-        if(vote_acmw > 0)
-            results_array.push(['ACM-W', vote_acmw]);
-
-        console.log(results_array);
-
-        var data = google.visualization.arrayToDataTable(results_array);
-
-        var options = {
-          title: 'What is the best committee?'
-        };
-
-        document.getElementById("piechart").innerHTML = "";
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-    }
-}
-
-//google.visualization.events.addListener(chart, 'ready', function() {
-//    console.log("In listener");
-//    drawChart();
-//});
 
 $(window).load(function() {
     drawChartPoll();
