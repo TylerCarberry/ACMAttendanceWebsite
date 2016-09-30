3000f916d66e3526ff7e6b5c488d31ca0b31312a
tylercarberry <tylercarberry@gmail.com>
2016-09-30 19:13:08 -0400

    Add comments

--- public/Scripts/FireBase.js
+++ public/Scripts/FireBase.js
@@ -7,28 +7,27 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
+// Get the number of people signed into the meeting
 var signed_in_ref = firebase.database().ref('signed_in_count/');
 signed_in_ref.on('value', function(snapshot) {
     var signed_in_value = snapshot.val();
     document.getElementById("signed_in").innerHTML = "How many people signed into this meeting: " + signed_in_value;
 });
 
-
+// Get the number of new members
 var new_members_ref = firebase.database().ref('new_members_count/');
 new_members_ref.on('value', function(snapshot) {
     var first_value = snapshot.val();
     document.getElementById("first_meeting").innerHTML = "Number of new members: " + first_value;
 });
 
+// Get the short url (goo.gl) of the Google Form
 var poll_url_ref = firebase.database().ref('poll_url/');
 poll_url_ref.on('value', function(snapshot) {
     var poll_url = snapshot.val();
     document.getElementById("poll_url").innerHTML = poll_url;
     $("#poll_qr").attr("src", poll_url+".qr");
-    
 });
-
 
 var poll_question = "Loading poll question...";
 var poll_results = null;
@@ -48,6 +47,7 @@ number.on('value', function(snapshot) {
     drawChartPoll();
 });
 
+// Draw the pie chart with the results of the poll
 function drawChartPoll() {
     if (poll_results != null) {
         var results_array = [[poll_question, 'Votes']];
@@ -57,23 +57,28 @@ function drawChartPoll() {
                 var name = poll_results[key].name;
                 var votes = poll_results[key].votes;
                 
+                // Add the vote name and count to the array
+                // Don't include it if nobody has voted for it
                 if(votes > 0)
                     results_array.push([name, votes]);
             }
         }
 
         var data = google.visualization.arrayToDataTable(results_array);
+        
+        // Include the poll title in the pie chart
         var options = {
           title: poll_question
         };
 
         document.getElementById("piechart").innerHTML = "";
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
         chart.draw(data, options);
     }
 }
 
+// When the page has loaded, draw the piechart
+// This is needed incase Firebase has responded before the page has fully loaded
 $(window).load(function() {
     drawChartPoll();
 });
