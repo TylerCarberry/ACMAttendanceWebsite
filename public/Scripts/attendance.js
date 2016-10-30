6177815bc0d3f80561a0013d1fa2749d6c4ff024
tylercarberry <tylercarberry@gmail.com>
2016-10-30 17:37:17 -0400

    Draw poll pie chart
    After the poll results are loaded, draw the pie chart

--- public/Scripts/attendance.js
+++ public/Scripts/attendance.js
@@ -6,6 +6,7 @@ var theData = null;
 var signed_in_count = 0;
 var new_member_count = 0;
 var poll_results = {};
+var poll_question = "Who is going to win the World Series?"
 
 function init() {
 Tabletop.init( { key: public_spreadsheet_url,
@@ -40,10 +41,46 @@ function calculateResults(data, tabletop) {
         
     }
     
-    displayResults()
+    updateSignInCount()
+    drawChartPoll()
 }
 
-function displayResults() {
+function updateSignInCount() {
     document.getElementById("first_meeting").innerHTML = "Number of new members: " + new_member_count;
     document.getElementById("signed_in").innerHTML = "How many people signed into this meeting: " + signed_in_count;
 }
+
+// Draw the pie chart with the results of the poll
+function drawChartPoll() {
+    if (poll_results != null) {
+        var results_array = [[poll_question, 'Votes']];
+        
+        for (var key in poll_results) {
+            if (poll_results.hasOwnProperty(key)) {
+                var votes = poll_results[key];
+                
+                // Add the vote name and count to the array
+                // Don't include it if nobody has voted for it
+                if(votes > 0)
+                    results_array.push([key, votes]);
+            }
+        }
+
+        var data = google.visualization.arrayToDataTable(results_array);
+        
+        // Include the poll title in the pie chart
+        var options = {
+          title: poll_question
+        };
+
+        document.getElementById("piechart").innerHTML = "";
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+    }
+}
+
+// When the page has loaded, draw the piechart
+// This is needed in case the attendance has loaded before the page
+$(window).load(function() {
+    drawChartPoll();
+});
